bf02c25e9020aa2d762dc24f0ffdd32735d5f90a
kazumi0128 <sh000128sh@gmail.com>
2025-01-14 23:37:39 +0900

    あいさつアプリ課題完了

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
+use App\Http\Controllers\GreetingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/comments/morning', [GreetingController::class, 'morning']);
+Route::get('/comments/afternoon', [GreetingController::class, 'afternoon']);
+Route::get('/comments/evening', [GreetingController::class, 'evening']);
+Route::get('/comments/night', [GreetingController::class, 'night']);
+Route::get('/comments/freeword/{message}', [GreetingController::class, 'freeword']);
+Route::get('/comments/random', [GreetingController::class, 'random']);
